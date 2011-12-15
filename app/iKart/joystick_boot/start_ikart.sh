964b558fa2e585fc5f409079b547dc39e257d956
Marco Randazzo <marco.randazzo@iit.it>
2011-12-15 18:22:24 +0000

    faster script

--- app/iKart/joystick_boot/start_ikart.sh
+++ app/iKart/joystick_boot/start_ikart.sh
@@ -8,18 +8,12 @@ if [ $ret1 -eq 101 ]; then
     sudo -u icub -i yarp server &
     sleep 2
     sudo -u icub -i iCubInterface --context iKart --config conf/iKart.ini &
-    sleep 2
     sudo -u icub -i iKartCtrl --no_compass &
-    sleep 2
     sudo -u icub -i joystickCtrl --context joystickCtrl --from conf/xbox_linux.ini --silent &
-    sleep 2
+    sleep 1
     sudo -u icub -i yarp connect /joystickCtrl:o /ikart/joystick:i
 elif [ $ret1 -eq 0 ]; then
-    echo "error 0" 
-elif [ $ret1 -eq -1 ]; then
-    echo "error -1" 
-elif [ $ret1 -eq -2 ]; then
-    echo "error -2" 
-else
     echo "No joystick boot requested" 
+else
+    echo "Unknown error" 
 fi

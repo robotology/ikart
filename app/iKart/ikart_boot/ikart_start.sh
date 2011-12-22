adaedd4c611b8ce006be6dfc24983c53a1a57b43
Marco Randazzo <marco.randazzo@iit.it>
2011-12-22 15:47:37 +0000

    scripts updated

--- app/iKart/ikart_boot/ikart_start.sh
+++ app/iKart/ikart_boot/ikart_start.sh
@@ -9,7 +9,7 @@ if [ $ret1 -eq 101 ]; then
     sleep 2
     sudo -u icub -i iCubInterface --context iKart --config conf/iKart.ini &
     sudo -u icub -i iKartCtrl --no_compass &
-    sudo -u icub -i joystickCtrl --context joystickCtrl --from conf/xbox_linux.ini --silent &
+    sudo -u icub -i joystickCtrl --context joystickCtrl --from conf/ikart.ini --silent &
     sleep 1
     sudo -u icub -i yarp connect /joystickCtrl:o /ikart/joystick:i
 elif [ $ret1 -eq 0 ]; then

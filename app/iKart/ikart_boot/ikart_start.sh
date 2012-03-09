04cb2e7ecad3031d9ba2d934d29bc981cbff7d99
Marco Randazzo <marco.randazzo@iit.it>
2012-03-09 09:17:00 +0000

    added autoconnect option, removed delays

--- app/iKart/ikart_boot/ikart_start.sh
+++ app/iKart/ikart_boot/ikart_start.sh
@@ -7,11 +7,9 @@ if [ $ret1 -eq 101 ]; then
     echo "joystick boot requested" 
     sudo -u icub -i yarp server &
     sleep 2
+    sudo -u icub -i joystickCtrl --context joystickCtrl --from conf/ikart.ini --silent --force_configuration &
     sudo -u icub -i iCubInterface --context iKart --config conf/iKart.ini &
-    sudo -u icub -i iKartCtrl --no_compass &
-    sudo -u icub -i joystickCtrl --context joystickCtrl --from conf/ikart.ini --silent &
-    sleep 1
-    sudo -u icub -i yarp connect /joystickCtrl:o /ikart/joystick:i
+    sudo -u icub -i iKartCtrl --no_compass --joystick_connect &
 elif [ $ret1 -eq 0 ]; then
     echo "No joystick boot requested" 
 else

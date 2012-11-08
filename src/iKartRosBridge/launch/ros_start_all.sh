9758395f223c172d971a555e2a023f655ec5050d
Marco Randazzo <marco.randazzo@iit.it>
2012-11-08 19:09:58 +0000

    script updated

--- src/iKartRosBridge/launch/ros_start_all.sh
+++ src/iKartRosBridge/launch/ros_start_all.sh
@@ -1,9 +1,11 @@
 roscore &
 sleep 3
-iKartRosBridge & 
+#iKartRosBridge & 
+iKartRosBridge --no_odom_tf &
 sleep 3
 $IKART_ROS_BRIDGE/launch/load_map.sh $IKART_ROS_BRIDGE/../../../iKart/app/iKart/maps/rbcsLab2 &
 sleep 3
+#roslaunch $IKART_ROS_BRIDGE/launch/ikart_localize.launch &
 roslaunch $IKART_ROS_BRIDGE/launch/ikart_laseronly_localize.launch &
 sleep 3
 roslaunch $IKART_ROS_BRIDGE/launch/rviz_navigate.launch &
